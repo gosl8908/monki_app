9414f0a318ce312fb65fd5c2b3c9249dd008948a
gosl8908 <gosl8908@gmail.com>
2024-05-30 12:18:04 +0900

    폴더 변경

--- project/결제.js
+++ project/결제.js
@@ -20,10 +20,11 @@ const serverUrl = "http://localhost:4723";
 
   // 검색 및 아이템 선택
   await clickElement(driver, uiSelectorText("검색"));
-  await wait(3 * 1000);
+  await wait(5 * 1000);
   await clickElement(driver, uiSelectorText("번개"));
-  await wait(3 * 1000);
+  await wait(5 * 1000);
   await clickElement(driver, uiSelectorText("번개맛집 안양지점"));
+  console.log('검색 성공')
 
   // 대기 10초
   await wait(10 * 1000);
@@ -31,6 +32,7 @@ const serverUrl = "http://localhost:4723";
   // 메뉴 선택 및 장바구니 담기
   await scroll(driver, 500, 1500, 500, 0);
   await clickElement(driver, uiSelectorText("비빔면"));
+  console.log('메뉴 상세 진입 성공')
 
   // 대기 10초
   await wait(10 * 1000);
@@ -38,41 +40,41 @@ const serverUrl = "http://localhost:4723";
   // 옵션 선택 및 결제 진행
   await scroll(driver, 500, 1500, 500, 0);
   await clickElement(driver, uiSelectorText("기본"));
-  await wait(3 * 1000);
+  await wait(5 * 1000);
   await clickElement(driver, uiSelectorText("8,000원 장바구니 담기"));
-  await wait(3 * 1000);
+  await wait(5 * 1000);
   await clickElement(driver, uiSelectorText("장바구니 보기"));
-  await wait(3 * 1000);
+  await wait(5 * 1000);
   await clickElement(driver, uiSelectorText("매장식사 주문"));
+  console.log('메뉴 장바구니 담기 성공')
 
   // 대기 10초
-  await wait(10000);
   await wait(10 * 1000);
 
   // 쿠폰 사용 및 결제 방식 선택
   await scroll(driver, 500, 2500, 500, 0);
   await clickElement(driver, uiSelectorText("모두사용"));
-  await wait(3 * 1000);
+  await wait(5 * 1000);
   await clickElement(driver, uiSelectorText("간편결제"));
-  await wait(3 * 1000);
+  await wait(5 * 1000);
 
   await clickElement(
     driver,
     uiSelectorText("개인정보 제3자 제공 내용 및 결제에 동의합니다.")
   );
-  await wait(3 * 1000);
+  await wait(5 * 1000);
   await clickElement(driver, uiSelectorText("결제하기"));
   await wait(15 * 1000);
   await scroll(driver, 500, 1300, 500, 100);
   await clickElement(driver, uiSelectorBtnText("결제"));
-  await wait(3 * 1000);
+  await wait(5 * 1000);
 
   // 결제 비밀번호 입력
   const password = ["9", "4", "0", "5", "1", "3"];
   for (const digit of password) {
     await clickElement(
       driver,
-      3`android=new UiSelector().className("android.widget.Button").text("${digit}")`
+      `android=new UiSelector().className("android.widget.Button").text("${digit}")`
     );
     await wait(1 * 1000);
   }
@@ -82,7 +84,7 @@ const serverUrl = "http://localhost:4723";
   try {
     const orderCompleteText = await driver.$(orderCompleteTextSelector);
     await orderCompleteText.waitForExist({ timeout: 30 * 1000 });
-    console.log("주문완료 텍스트가 나타났습니다.");
+    console.log("결제완료 & 주문완료 텍스트가 나타났습니다.");
   } catch (error) {
     console.log("주문완료 텍스트가 30초 내에 나타나지 않았습니다.");
   }
