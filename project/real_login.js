0f207aae61cff4254e374a26dd5667eb3fe94afa
gosl8908 <gosl8908@gmail.com>
2024-08-06 16:27:33 +0900

    code 수정

--- project/real_login.js
+++ project/real_login.js
@@ -1,35 +1,29 @@
 const { remote } = require("webdriverio");
-const { options } = require("../config");
+const { options } = require("../config.js");
 const { loginModule } = require('../module/manager.module.js');
 const {
   clickElement,
   scroll,
   wait,
   uiSelectorText,
-  enterText,
-} = require("../utils");
+} = require("../utils.js");
 
 const serverUrl = "http://localhost:4723";
-const TIMEOUT = 5000; // 전역 타임아웃 값
 
-// 공통 작업을 위한 유틸리티 함수들
-async function login(driver, email, password) {
-  await enterText(driver, '//android.widget.EditText[@text="이메일을 입력해 주세요"]', email);
-  await enterText(driver, '//android.widget.EditText[@text="비밀번호를 입력해 주세요"]', password);
-  await clickElement(driver, uiSelectorText("로그인"));
-}
-
-async function waitForTextAndClick(driver, text) {
-  const selector = uiSelectorText(text);
+async function login(driver) {
+  const loginBtnSelector = uiSelectorText("로그인");
   try {
-    const element = await driver.$(selector);
-    await element.waitForExist({ timeout: TIMEOUT });
-    await element.click();
-    console.log(`"${text}" 텍스트를 찾고 클릭했습니다.`);
+    const loginBtn = await driver.$(loginBtnSelector);
+    if (await loginBtn.isDisplayed()) {
+      await loginModule.login('hskang@monki.net', 'gotjd0215!');
+    } else {
+      await handleNextStep();
+    }
   } catch (error) {
-    console.log(`"${text}" 텍스트를 찾지 못했습니다: ${error.message}`);
+    await handleNextStep();
   }
 }
+
 
 async function logout(driver) {
   await clickElement(driver, uiSelectorText("My먼키"));
@@ -55,6 +49,7 @@ async function logout(driver) {
   await wait(2000);
 }
 
+
 async function verifyLogout(driver) {
   const loginTextSelector = uiSelectorText("로그인");
   try {
@@ -70,13 +65,12 @@ async function verifyLogout(driver) {
   const driver = await remote(options);
 
   try {
-    // 초기 대기
-    await wait(5000);
+    await wait(5 * 1000);
 
-    // 로그인
-    await login(driver, 'hskang@monki.net', 'gotjd0215!');
-
-    // 주문 완료 확인
+    /* 로그인 */
+    await login(driver);
+    
+    /* 하단 배너 확인 */
     await waitForTextAndClick(driver, "오늘하루 그만보기");
 
     // 내 계정 메뉴에서 로그아웃
@@ -86,8 +80,9 @@ async function verifyLogout(driver) {
     await verifyLogout(driver);
 
   } catch (error) {
-    console.error("테스트 실행 중 오류 발생: ", error.message);
+    console.error("Error occurred:", error);
   } finally {
-    await driver.deleteSession(); // 세션 종료
+    await driver.deleteSession();
+    console.log("Driver session ended.");
   }
 })();
