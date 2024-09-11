8346865f962ee5ec8d164ded1c8d21d9dd9e7173
gosl8908 <gosl8908@gmail.com>
2024-09-11 17:45:09 +0900

    code 수정

--- scheduled/app/githubAction.js
+++ scheduled/app/githubAction.js
@@ -89,7 +89,7 @@ let TestFails = []; // 실패 원인을 저장할 변수
         }
         await Module.emailModule.email({
             TestFails,
-            EmailTitle: `[${env.EmailTitle}]`,
+            EmailTitle: `[${env.AppEmailTitle}]`,
             TestRange: '1. 지점 매장식사 결제',
             Screenshots,
         });
